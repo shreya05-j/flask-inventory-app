105e03d861e23ec5e7e9064a71c33ff29107dafa
Shreya Mahajan <yashshreya21@gmail.com>
2025-07-02 19:24:57 +0530

    Update customer_behavior.py

--- customer_behavior.py
+++ customer_behavior.py
@@ -1,13 +1,24 @@
-def analyze_heatmap(beacon_data):
-    """
-    Analyze beacon data to create a heatmap.
-    """
-    # TODO: Implement real analysis
-    return {'hotspots': ['Aisle 1', 'Aisle 3']}
+# customer_behavior.py
+from flask import Blueprint, render_template, request
+import random
 
-def recommend_products(customer_path):
-    """
-    Recommend products based on in-store path.
-    """
-    # TODO: Implement real recommender
-    return ['Milk', 'Eggs']
+customer_behavior_bp = Blueprint('customer_behavior', __name__)
+
+# Mock customer path and dwell time data generator
+def generate_mock_customer_data():
+    zones = ['Entrance', 'Aisle 1', 'Aisle 2', 'Aisle 3', 'Checkout']
+    path = random.sample(zones, k=len(zones))
+    dwell_times = {zone: random.randint(1, 10) for zone in path}  # minutes
+    return path, dwell_times
+
+@customer_behavior_bp.route('/customer_behavior', methods=['GET', 'POST'])
+def customer_behavior_view():
+    # Generate simulated data
+    customer_path, dwell_data = generate_mock_customer_data()
+
+    return render_template(
+        'customer_behavior.html',
+        path=customer_path,
+        dwell_times=dwell_data,
+        heatmap_img='heatmap.png'  # static image
+    )
