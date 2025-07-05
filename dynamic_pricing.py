2bd3540ee3830cdbb021d9bf282dfdedbc835bb7
Shreya Mahajan <yashshreya21@gmail.com>
2025-07-05 19:35:17 +0530

    Update dynamic_pricing.py

--- dynamic_pricing.py
+++ dynamic_pricing.py
@@ -6,6 +6,16 @@ from sklearn.linear_model import LinearRegression
 # Blueprint for modular route registration
 dynamic_pricing_bp = Blueprint('dynamic_pricing', __name__)
 
+# Sample training data (demand, inventory, competitor_price)
+X = np.array([
+    [10, 50, 90],
+    [20, 40, 95],
+    [15, 30, 100],
+    [30, 20, 105],
+    [25, 10, 110]
+])
+y = np.array([100, 105, 110, 120, 130])
+
 # Train the simple ML model
 model = LinearRegression()
 model.fit(X, y)
